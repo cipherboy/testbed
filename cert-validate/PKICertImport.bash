f8bc843039dcce23bac0675a8786fe15b58f7800
Alexander Scheel <ascheel@redhat.com>
2019-01-24 16:06:52 -0500

    Updates to PKICertImport to make it use the original NSS DB
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- cert-validate/PKICertImport.bash
+++ cert-validate/PKICertImport.bash
@@ -19,11 +19,7 @@ function PKICertImport() {
 
     ## [ globals ] ##
 
-    # Temporary directory base location.
-    local TMPBASE=""
-
-    # Path to certificate; parsed from arguments, then updated to reflect
-    # location under TMPBASE.
+    # Path to certificate; parsed from arguments.
     local CERT_PATH=""
 
     # User-given nickname for the certificate.
@@ -69,110 +65,6 @@ function PKICertImport() {
             echo "v:" "$@" 1>&2
         fi
     }
-
-    # mk_secure_tmp fills the TMPBASE variable with the path to a directory
-    # we can use that has permissions restricted to our current user. This
-    # will be where we move the NSS DB and the certificate to.
-    #
-    # Failures are fatal, so use exit instead of return to simplify error
-    # handling.
-    function __mk_secure_tmp() {
-        local tmpdir="$TMPDIR"
-        local ret=0
-
-        # Prefer /dev/shm over /tmp: /dev/shm is less frequently backed by
-        # a physical disk than /tmp. However, if TEMPDIR is explicitly set,
-        # respect it.
-        if [ "x$tmpdir" == "x" ] && [ -d "/dev/shm" ]; then
-            tmpdir="/dev/shm"
-        elif [ "x$tmpdir" == "x" ] && [ -d "/tmp" ]; then
-            tmpdir="/tmp"
-        fi
-
-        # Create the temporary directory.
-        TMPBASE="$(mktemp --directory --tmpdir="$tmpdir" 2>&1)"
-        ret="$?"
-
-        if (( ret != 0 )); then
-            # ret being non-zero is a definite failure in mktemp.
-            __e "Return from mktemp was non-zero: $ret" 1>&2
-            __e "$TMPBASE" 1>&2
-            __e "Perhaps specify TMPDIR in the environment?" 1>&2
-            exit 1
-        elif [ ! -d "$TMPBASE" ]; then
-            # Theoretically mktemp should exit with zero status only when
-            # creating the directory was successful; however, since we also
-            # redirect stderr to join stdout, the output of mktemp could
-            # include a warning, causing TMPBASE to not be a valid directory.
-
-            # This ensures we don't continue if that is the case.
-            __e "Return from mktemp was zero but invalid directory:" 1>&2
-            __e "$TMPBASE" 1>&2
-            __e "Perhaps specify TMPDIR in the environment?" 1>&2
-            exit 1
-        fi
-
-        # We've validated that TMPBASE is now a valid directory. Since
-        # we created it, we have ownership. Restrict access to only this
-        # user as the original NSS DB might have private keys which we want
-        # to keep secure when copying. This ensures other users cannot access
-        # our copied files.
-
-        local user=""
-        local group=""
-
-        # Acquire curreent username.
-        user="$(id --user --name 2>&1)"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "id exited with non-zero result: $ret" 1>&2
-            __e "Unable to get current user's name." 1>&2
-            __rm_secure_tmp
-            exit 1
-        fi
-
-        # Acquire curreent primary group.
-        group="$(id --group --name 2>&1)"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "id exited with non-zero result: $ret" 1>&2
-            __e "Unable to get current user's name." 1>&2
-            __rm_secure_tmp
-            exit 1
-        fi
-
-        # Change ownership prior to permissions; theoretically these should
-        # already be the current owner.
-        chown "$user:$group" -R "$TMPBASE"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "Return from chown on $TMPBASE was non-zero: $ret" 1>&2
-            __rm_secure_tmp
-            exit 1
-        fi
-
-        # Restrict access only to the owner, preventing any group and world
-        # access.
-        chmod 700 -R "$TMPBASE"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "Return from chmod on $TMPBASE was non-zero: $ret" 1>&2
-            __rm_secure_tmp
-            exit 1
-        fi
-
-        return 0
-    }
-
-    ## rm_secure_tmp removes the temporary directory if present.
-    function __rm_secure_tmp() {
-        if [ -d "$TMPBASE" ]; then
-            # TODO: Determine if we should shred the contents if the shred
-            # utility is present; otherwise, could fall back on dd...
-            rm -rf "$TMPBASE"
-        fi
-    }
-
 
     ## [ core commands ] ##
 
@@ -288,34 +180,10 @@ function PKICertImport() {
         echo "--hsm, -h <name>: name of the HSM to use"
         echo ""
         echo "Environment variables:"
-        echo "TMPDIR: specify a location to place a temporary directory"
         echo "VERBOSE: see certutil commands being run"
         echo ""
         echo "For more information about these options, refer to the" \
              "certutil documentation."
-    }
-
-    # Copy NSS DB and Certificate to temporary location. Errors are fatal;
-    # uses exit instead of return for simplified error handling.
-    function _copy_to_temp() {
-        local ret=0
-
-        NSSDB_TEMP="$TMPBASE/nssdb"
-        cp -r "$NSSDB" "$NSSDB_TEMP"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "Copying $NSSDB to temporary directory failed: $ret"
-            exit 1
-        fi
-
-        local cert_temp="$TMPBASE/cert"
-        cp "$CERT_PATH" "$cert_temp"
-        ret=$?
-        if (( ret != 0 )); then
-            __e "Copying $CERT_PATH to temporary directory failed: $ret"
-            exit 1
-        fi
-        CERT_PATH="$cert_temp"
     }
 
     # Import a certificate into the NSS DB specified on $1. When a HSM is
@@ -436,15 +304,12 @@ function PKICertImport() {
     #   - [print help if required]
     # - Create working directory
     # - Copy files into the working directory
-    # - Import certificate into temporary NSS DB
-    # - Modify trust flags in temporary NSS DB
-    # - Validate certificate in temporary NSS DB
-    #   - [on failure, quit]
-    # - Import certificate into real NSS DB
-    # - Modify trust flags on real NSS DB
+    # - Import certificate into NSS DB
+    # - Validate certificate in NSS DB
+    #   - [on failure, remove from NSS DB]
     #
-    # Import and Modify are both handled by _import_cert, verification is
-    # handled by _verify_cert, and removal is handled by _remove_cert.
+    # Import is handled by _import_cert, verification is handled by
+    # _verify_cert, and removal is handled by _remove_cert.
 
     _parse_args "$@"
     ret="$?"
@@ -453,36 +318,16 @@ function PKICertImport() {
         exit 1
     fi
 
-    __mk_secure_tmp
-    _copy_to_temp
-
-    _import_cert "$NSSDB_TEMP"
-    _verify_cert "$NSSDB_TEMP"
+    _import_cert "$NSSDB"
+    _verify_cert "$NSSDB"
     ret=$?
 
-    # Note that we always have to remove the certificate from the temporary
-    # NSS DB--even when import succeeded. Otherwise, if we're using a HSM,
-    # the certificate will fail to be imported into the real NSS DB because
-    # it already exists on the HSM.
-    _remove_cert "$NSSDB_TEMP"
-
-    # Check if the verification failed.
+    # Check if the verification failed. If it did, remove it from the NSS DB.
     if (( ret != 0 )); then
         __e "Verification of certificate failed!"
-        __rm_secure_tmp
+        _remove_cert "$NSSDB"
         exit 1
     fi
-
-    # Since verification succeeded, we can now import the certificate into
-    # the real NSS Database. If verification did not succeed, we'd have
-    # exited prior to reaching this point.
-    _import_cert "$NSSDB"
-
-    # Note that we only remove the temporary directory when the script
-    # succeeds; or validation failed. For other unexpected errors, the
-    # temporary directory persists, enabling the runner to debug any
-    # issues if necessary (when coupled with VERBOSE=1 flag).
-    __rm_secure_tmp
 }
 
 PKICertImport "$@"
