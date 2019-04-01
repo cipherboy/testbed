a590ec8cfe36b45be4777f234f9df11cff338619
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-04-01 14:41:04 -0400

    Fix repo creation
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- rpm-validate/post.sh
+++ rpm-validate/post.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+# Add our repository information to yum so it can find it.
+cp repos-testing.repo /etc/yum.repos.d/repos-testing.repo
+
 # createrepo will build a valid RPM repository for use with yum from whatever
 # RPM files are placed in that folder.
 cd /repos/testing
 createrepo .
-
-# Add our repository information to yum so it can find it.
-cp repos-testing.repo /etc/yum.repos.d/repos-testing.repo
 
 # Clean caches. Sometimes this is necessary if things have changed, e.g.,
 # gpgcheck, etc.
