460218a6176cb90deb66019b18477428f913efc8
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-05-21 09:10:58 -0400

    Add test case for JSS crash
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ jss-crash-reproducer/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export CLASSPATH="/usr/share/java/slf4j/api.jar:/usr/share/java/apache-commons-codec.jar:/usr/share/java/apache-commons-lang.jar:/usr/share/java/jaxb-api.jar:/usr/lib/java/jss4.jar"
+
+javac -classpath "$CLASSPATH" Reproducer.java && java -classpath "$CLASSPATH:." Reproducer
