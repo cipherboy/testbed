8e955e0e0b81263dacb5914695c9f3d06a8e2ae0
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-06-20 12:08:51 -0400

    [jss] Initialize CryptoManager
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- jss-crash-reproducer/build.sh
+++ jss-crash-reproducer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export CLASSPATH="/usr/share/java/slf4j/api.jar:/usr/share/java/apache-commons-codec.jar:/usr/share/java/apache-commons-lang.jar:/usr/share/java/jaxb-api.jar:/usr/lib/java/jss4.jar"
+export CLASSPATH="/usr/share/java/slf4j/simple.jar:/usr/share/java/slf4j/api.jar:/usr/share/java/apache-commons-codec.jar:/usr/share/java/apache-commons-lang.jar:/usr/share/java/jaxb-api.jar:/usr/lib/java/jss4.jar"
 
-javac -classpath "$CLASSPATH" Reproducer.java && java -classpath "$CLASSPATH:." Reproducer
+javac -classpath "$CLASSPATH" Reproducer.java && java -classpath "$CLASSPATH:." Reproducer "$@"
