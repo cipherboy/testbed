2ba78564603019024f3cde31366d405edd27de99
Alexander Scheel <alex.scheel@hashicorp.com>
2022-05-26 12:10:11 -0400

    First working implementation
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- nss-transit-byok/main.c
+++ nss-transit-byok/main.c
@@ -5,6 +5,8 @@
 #include <pk11func.h>
 #include <nspr.h>
 #include <pkcs11.h>
+#include <pkcs11t.h>
+#include <base64.h>
 #include <nss.h>
 
 #include <stdbool.h>
@@ -125,6 +127,21 @@ int doImport(int argc, int *offset, const char **argv) {
         return 1;
     }
 
+    CK_BBOOL cktrue = CK_TRUE;
+    SECItem emptyItem = {0, &cktrue, sizeof(cktrue)};
+    if (PK11_WriteRawAttribute(PK11_TypePubKey, pkey, CKA_WRAP, &emptyItem) != SECSuccess) {
+        PRErrorCode code = PORT_GetError();
+        const char *message = PORT_ErrorToString(code);
+        fprintf(stderr, "PK11_WriteRawAttribute(pkey, CKA_WRAP) failed with code (%d): %s\n", code, message);
+        return 1;
+    }
+    if (PK11_WriteRawAttribute(PK11_TypePubKey, pkey, CKA_ENCRYPT, &emptyItem) != SECSuccess) {
+        PRErrorCode code = PORT_GetError();
+        const char *message = PORT_ErrorToString(code);
+        fprintf(stderr, "PK11_WriteRawAttribute(pkey, CKA_ENCRYPT) failed with code (%d): %s\n", code, message);
+        return 1;
+    }
+
     if (PK11_SetPublicKeyNickname(pkey, name) != SECSuccess) {
         PRErrorCode code = PORT_GetError();
         const char *message = PORT_ErrorToString(code);
@@ -214,7 +231,6 @@ int doGenerate(int argc, int *offset, const char **argv) {
             return 1;
         }
 
-
         SECItem *publicItem = PK11_DEREncodePublicKey(public);
         fprintf(stdout, "Generated Nickname: %s\n", PK11_GetPublicKeyNickname(public));
         fprintf(stdout, "Generated Key:\n%s\n", HexFormatByteBuffer(publicItem->data, publicItem->len, 0));
@@ -223,7 +239,7 @@ int doGenerate(int argc, int *offset, const char **argv) {
     return 0;
 }
 
-void parseExportArgs(int argc, int *offset, const char **argv, int *aes_bits, const char **signWith, const char **toSign) {
+void parseExportArgs(int argc, int *offset, const char **argv, int *aes_bits, int *hash_bits, int *mgf_bits, const char **signWith, const char **toSign) {
     for (; *offset < argc; *offset = (*offset) + 1) {
         if (strcmp("-h", argv[*offset]) == 0) {
             *offset = -1;
@@ -242,6 +258,34 @@ void parseExportArgs(int argc, int *offset, const char **argv, int *aes_bits, co
                 *offset = -1;
                 return;
             }
+        } else if (strcmp("-h", argv[*offset]) == 0) {
+            *offset += 1;
+            if (*offset >= argc) {
+                fprintf(stderr, "Option -h requires an argument (number of bits in hash function); none was given\n");
+                *offset = -1;
+                return;
+            }
+
+            *hash_bits = atoi(argv[*offset]);
+            if (*hash_bits != 160 && *hash_bits != 224 && *hash_bits != 256 && *hash_bits != 384 && *hash_bits != 512) {
+                fprintf(stderr, "Option -h only accepts 160/224/256/384/512 bits; got %s -> %d\n", argv[*offset], *aes_bits);
+                *offset = -1;
+                return;
+            }
+        } else if (strcmp("-m", argv[*offset]) == 0) {
+            *offset += 1;
+            if (*offset >= argc) {
+                fprintf(stderr, "Option -m requires an argument (number of bits in MGF hash function); none was given\n");
+                *offset = -1;
+                return;
+            }
+
+            *mgf_bits = atoi(argv[*offset]);
+            if (*hash_bits != 160 && *hash_bits != 224 && *hash_bits != 256 && *hash_bits != 384 && *hash_bits != 512) {
+                fprintf(stderr, "Option -m only accepts 160/224/256/384/512 bits; got %s -> %d\n", argv[*offset], *aes_bits);
+                *offset = -1;
+                return;
+            }
         } else if (*signWith == NULL) {
             *signWith = argv[*offset];
         } else if (*toSign == NULL) {
@@ -252,49 +296,26 @@ void parseExportArgs(int argc, int *offset, const char **argv, int *aes_bits, co
     }
 }
 
-int doExport(int argc, int *offset, const char **argv) {
-    int aes_bits = 128;
-    const char *signWith = NULL;
-    const char *toSign = NULL;
-
-    parseExportArgs(argc, offset, argv, &aes_bits, &signWith, &toSign);
-    if (*offset == -1 || signWith == NULL || toSign == NULL) {
-        fprintf(stderr, "Usage: %s export [-b AES_BITS] SIGNWITH TOSIGN\n", argv[0]);
-        return 2;
-    }
-
-    // Grab the slot.
-    PK11SlotInfo *slot = PK11_GetInternalKeySlot();
-    if (slot == NULL) {
-        PRErrorCode code = PORT_GetError();
-        const char *message = PORT_ErrorToString(code);
-        fprintf(stderr, "PK11_GetInternalKeySlot() failed with code (%d): %s\n", code, message);
-        return 1;
-    }
-
-    // Find the keys.
+SECKEYPublicKey *findPublicKeyWithName(PK11SlotInfo *slot, char *signWith) {
     SECKEYPublicKey *outer = NULL;
-    SECKEYPublicKeyList *pub_list = PK11_ListPublicKeysInSlot(slot, (char *)signWith);
+    SECKEYPublicKeyList *pub_list = PK11_ListPublicKeysInSlot(slot, signWith);
     SECKEYPublicKeyListNode *pub_head = PUBKEY_LIST_HEAD(pub_list);
     while (!PUBKEY_LIST_END(pub_head, pub_list)) {
         if (outer == NULL) {
             outer = pub_head->key;
         } else {
             fprintf(stderr, "Too many outer wrapping keys with same name: %s\n", signWith);
-            return 1;
+            return NULL;
         }
         pub_head = PUBKEY_LIST_NEXT(pub_head);
     }
 
-    if (outer == NULL) {
-        fprintf(stderr, "Unable to find outer wrapping key with name: %s\n", signWith);
-        return 1;
-    }
+    return outer;
+}
 
-    SECKEY_DestroyPublicKeyList(pub_list);
-
+SECKEYPrivateKey *findPrivateKeyWithName(PK11SlotInfo *slot, char *toSign) {
     SECKEYPrivateKey *candidate = NULL;
-    SECKEYPrivateKeyList *priv_list = PK11_ListPrivKeysInSlot(slot, (char *)toSign, NULL);
+    SECKEYPrivateKeyList *priv_list = PK11_ListPrivKeysInSlot(slot, toSign, NULL);
     SECKEYPrivateKeyListNode *priv_head = PRIVKEY_LIST_HEAD(priv_list);
     while (!PRIVKEY_LIST_END(priv_head, priv_list)) {
         if (candidate == NULL) {
@@ -327,17 +348,144 @@ int doExport(int argc, int *offset, const char **argv) {
             SECItem *nextItem = PK11_DEREncodePublicKey(nextKey);
 
             fprintf(stderr, "First (%s->%s):\n%s\nSecond (%s->%s):\n%s\n", candidateName, candidatePublicName, HexFormatByteBuffer(candidateItem->data, candidateItem->len, 0), nextName, nextPublicName, HexFormatByteBuffer(nextItem->data, nextItem->len, 0));
-            return 1;
+            return NULL;
         }
         priv_head = PRIVKEY_LIST_NEXT(priv_head);
     }
 
-    if (candidate == NULL) {
-        fprintf(stderr, "Unable to find inner private key with name: %s\n", toSign);
+    return candidate;
+}
+
+CK_MECHANISM_TYPE hashBitsToMech(int bits) {
+    switch (bits) {
+        case 160:
+            return CKM_SHA_1;
+        case 224:
+            return CKM_SHA224;
+        case 256:
+            return CKM_SHA256;
+        case 384:
+            return CKM_SHA384;
+        case 512:
+            return CKM_SHA512;
+    }
+    return 0;
+}
+
+CK_RSA_PKCS_MGF_TYPE mgfBitsToMech(int bits) {
+    switch (bits) {
+        case 160:
+            return CKG_MGF1_SHA1;
+        case 224:
+            return CKG_MGF1_SHA256;
+        case 256:
+            return CKG_MGF1_SHA256;
+        case 384:
+            return CKG_MGF1_SHA384;
+        case 512:
+            return CKG_MGF1_SHA512;
+    }
+    return 0;
+}
+
+PK11SymKey *findSymmetricKeyWithName(PK11SlotInfo *slot, char *toSign) {
+    PK11SymKey *candidate = PK11_ListFixedKeysInSlot(slot, toSign, NULL);
+    return candidate;
+}
+
+int doExport(int argc, int *offset, const char **argv) {
+    int aes_bits = 128;
+    int hash_bits = 256;
+    int mgf_bits = 256;
+    const char *signWith = NULL;
+    const char *toSign = NULL;
+
+    parseExportArgs(argc, offset, argv, &aes_bits, &hash_bits, &mgf_bits, &signWith, &toSign);
+    if (*offset == -1 || signWith == NULL || toSign == NULL) {
+        fprintf(stderr, "Usage: %s export [-b AES_BITS] SIGNWITH TOSIGN\n", argv[0]);
+        return 2;
+    }
+
+    fprintf(stdout, "AES key bits: %d\n", aes_bits);
+    fprintf(stdout, "hash bits: %d\n", hash_bits);
+    fprintf(stdout, "mgf1 bits: %d\n", mgf_bits);
+
+    // Grab the slot.
+    PK11SlotInfo *slot = PK11_GetInternalKeySlot();
+    if (slot == NULL) {
+        PRErrorCode code = PORT_GetError();
+        const char *message = PORT_ErrorToString(code);
+        fprintf(stderr, "PK11_GetInternalKeySlot() failed with code (%d): %s\n", code, message);
         return 1;
     }
 
-    SECKEY_DestroyPrivateKeyList(priv_list);
+    // Find the keys.
+    SECKEYPublicKey *outer = findPublicKeyWithName(slot, (char *)signWith);
+    if (outer == NULL) {
+        fprintf(stderr, "Unable to find outer wrapping key with name: %s\n", signWith);
+        return 1;
+    }
+
+    SECKEYPrivateKey *asymCandidate = findPrivateKeyWithName(slot, (char *)toSign);
+    PK11SymKey *symCandidate = findSymmetricKeyWithName(slot, (char *)toSign);
+
+    if (asymCandidate == NULL && symCandidate == NULL) {
+        fprintf(stderr, "Unable to find inner private or asymmetric key with name: %s\n", toSign);
+        return 1;
+    } else if (asymCandidate != NULL && symCandidate != NULL) {
+        fprintf(stderr, "Found both inner private and asymmetric key with name: %s\n", toSign);
+        return 1;
+    }
+
+    // Now, wrap wrap wrap. Start by generating a new AES key for wrapping.
+    // It turns out that NSS only allows wrap->wrap and doesn't have a
+    // one-shot helper for this call.
+    PK11SymKey *transient = PK11_KeyGen(slot, CKM_AES_KEY_GEN, NULL, aes_bits/8, NULL);
+    if (transient == NULL) {
+        PRErrorCode code = PORT_GetError();
+        const char *message = PORT_ErrorToString(code);
+        fprintf(stderr, "Unable to generate transient wrapping key (%d): %s\n", code, message);
+        return 1;
+    }
+
+    SECItem wrappedTransient;
+    wrappedTransient.len = 4096;
+    wrappedTransient.data = PR_Calloc(wrappedTransient.len, sizeof(uint8_t));
+
+    CK_RSA_PKCS_OAEP_PARAMS oaep_params = {hashBitsToMech(hash_bits),
+                                           mgfBitsToMech(mgf_bits),
+                                           CKZ_DATA_SPECIFIED, NULL, 0};
+
+    SECItem param;
+    param.data = (unsigned char *)&oaep_params;
+    param.len = sizeof(oaep_params);
+
+    if (PK11_PubWrapSymKeyWithMechanism(outer, CKM_RSA_PKCS_OAEP, &param, transient, &wrappedTransient) != SECSuccess) {
+        PRErrorCode code = PORT_GetError();
+        const char *message = PORT_ErrorToString(code);
+        fprintf(stderr, "Unable to wrap transient wrapping key (%d): %s\n", code, message);
+        return 1;
+    }
+
+    SECItem wrappedCandidate;
+    wrappedCandidate.len = 40960;
+    wrappedCandidate.data = PR_Calloc(wrappedCandidate.len, sizeof(uint8_t));
+
+    if (asymCandidate != NULL) {
+        if (PK11_WrapPrivKey(slot, transient, asymCandidate, CKM_AES_KEY_WRAP_KWP, NULL, &wrappedCandidate, NULL) != SECSuccess) {
+            PRErrorCode code = PORT_GetError();
+            const char *message = PORT_ErrorToString(code);
+            fprintf(stderr, "Unable to wrap transient wrapping key (%d): %s\n", code, message);
+            return 1;
+        }
+    }
+
+    size_t totalLen = wrappedTransient.len + wrappedCandidate.len;
+    uint8_t *allData = calloc(wrappedTransient.len + wrappedCandidate.len, sizeof(uint8_t));
+    memcpy(allData, wrappedTransient.data, wrappedTransient.len);
+    memcpy(allData + wrappedTransient.len, wrappedCandidate.data, wrappedCandidate.len);
+
+    fprintf(stdout, "Wrapped data (%u + %u = %zu bytes):\n%s", wrappedTransient.len, wrappedCandidate.len, totalLen, BTOA_DataToAscii(allData, totalLen));
 
     return 0;
 }
